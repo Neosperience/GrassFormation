c6ce08c80d3f546ea0ac08fef1de64284796123a
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2018-10-30 13:06:43 +0100

    Refactor common patterns to a single class

---
+++ grassformation/greengrass_resource_handler.py
@@ -0,0 +1,116 @@
+# grassformation/greengrass_resource_handler.py
+
+''' Defines the lambda functions for managing CloudFormation custom resources of
+AWS Greengrass. '''
+
+import botocore
+from grassformation.utils import change_requires_update, filter_dictionary
+
+class CollectionHandler:
+    ''' Instances of this class manages Greengrass CloudFormation resource
+    requests.
+
+    Currently supported resources:
+    - FunctionDefinition
+    - LoggerDefinition
+    - ResourceDefinition
+    - SubscriptionDefinition
+    - DeviceDefinition
+    - CoreDefinition
+    '''
+
+    def __init__(self, logger, resource_collection_key, id_key,
+                 clean_resource_definition,
+                 create_aws_function, create_version_aws_function,
+                 update_aws_function, delete_aws_function):
+        '''
+        Initializes the resource collection handler.
+
+        Params:
+          - logger (logging.Logger): The logger instance.
+          - resource_collection_key (str): The key of the Greengrass resource
+            collection in the CloudFormation resource definition
+          - id_key (str): The key of the Greengrass resource id in update/delete
+            requests
+          - clean_resource_definition (func): The function that converts a
+            single instance of a CloudFormation resource to a parameter set
+            that can be passed to the AWS API.
+          - create_aws_function (func): The Greengrass API function responsible
+            for creating the resource definition.
+          - create_version_aws_function (func): The Greengrass API function responsible
+            for creating the resource definition version.
+          - update_aws_function (func): The Greengrass API function responsible
+            for updating the resource definition.
+          - delete_aws_function (func): The Greengrass API function responsible
+            for deleting the resource definition.
+        '''
+        self.logger = logger
+        self.resource_collection_key = resource_collection_key
+        self.id_key = id_key
+        self.clean_resource_definition = clean_resource_definition
+        self.create_aws_function = create_aws_function
+        self.create_version_aws_function = create_version_aws_function
+        self.update_aws_function = update_aws_function
+        self.delete_aws_function = delete_aws_function
+
+    def clean_resource_definition_collection(self, resource_definition_collection):
+        return [self.clean_resource_definition(res) for res in resource_definition_collection]
+
+    def create(self, event, context):
+        params = {}
+        params['Name'] = event['ResourceProperties']['Name']
+        initial_version = filter_dictionary(event['ResourceProperties'], [self.resource_collection_key])
+        if initial_version:
+            self.logger.info('Resource InitialVersion detected')
+            params['InitialVersion'] = {
+                self.resource_collection_key: self.clean_resource_definition_collection(initial_version[self.resource_collection_key])
+            }
+        response = self.create_aws_function(**params)
+        response.pop('ResponseMetadata', None)
+        physical_resource_id = response['Id']
+        return physical_resource_id, response
+
+    def update(self, event, context):
+        physical_resource_id = event['PhysicalResourceId']
+        response = {}
+        requires_new_version = self.resource_collection_key in params and \
+                               change_requires_update(self.logger,
+                                                      [self.resource_collection_key],
+                                                      event['OldResourceProperties'],
+                                                      event['ResourceProperties'])
+        if requires_new_version:
+            self.logger.info('Resource requires new version')
+            params = filter_dictionary(event['ResourceProperties'], [self.resource_collection_key])
+            params[self.resource_collection_key] = self.clean_resource_definition_collection(params[self.resource_collection_key])
+            params[self.id_key] = physical_resource_id
+            version_response = self.create_version_aws_function(**params)
+            version_response.pop('ResponseMetadata', None)
+            response['Version'] = version_response
+
+        requires_rename = change_requires_update(self.logger,
+                                                 ['Name'],
+                                                 event['OldResourceProperties'],
+                                                 event['ResourceProperties'])
+        if requires_rename:
+            self.logger.info('Resource is renamed')
+            params = {
+                self.id_key: physical_resource_id,
+                'Name': event['ResourceProperties']['Name']
+            }
+            self.update_aws_function(**params)
+
+        return physical_resource_id, response
+
+    def delete(self, event, context):
+        physical_resource_id = event['PhysicalResourceId']
+        if physical_resource_id == 'NONE':
+            return
+        try:
+            params = { self.id_key: physical_resource_id }
+            self.delete_aws_function(**params)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'IdNotFoundException':
+                self.logger.warning('Requested to delete non existing resource.')
+            else:
+                raise e
+        return
---
+++ grassformation/index.py
@@ -0,0 +1,168 @@
+# grassformation/index.py
+
+''' Defines the lambda functions for managing CloudFormation custom resources of
+AWS Greengrass. '''
+
+import botocore
+from grassformation.utils import crhelper
+from grassformation.utils import keypath
+from grassformation.utils import change_requires_update, filter_dictionary, val_to_bool
+from grassformation.greengrass_resource_handler import CollectionHandler
+
+# initialise logger
+logger = crhelper.log_config({"RequestId": "CONTAINER_INIT"})
+logger.info('Logging configured')
+# set global to track init failures
+init_failed = False
+
+try:
+    import boto3
+    greengrass_client = boto3.client('greengrass')
+    logger.info("Container initialization completed")
+except Exception as e:
+    logger.error(e, exc_info=True)
+    init_failed = e
+
+def core_handler(event, context):
+    ''' Lambda handler to manage AWS Greengrass CoreDefinition resources. '''
+    global logger
+    logger = crhelper.log_config(event)
+
+    def clean_core(core):
+        return keypath.replace(core, 'SyncShadow', lambda e: val_to_bool(e), inline=False)
+
+    handler = CollectionHandler(
+                    logger, 'Cores', 'CoreDefinitionId',
+                    clean_core,
+                    greengrass_client.create_core_definition,
+                    greengrass_client.create_core_definition_version,
+                    greengrass_client.update_core_definition,
+                    greengrass_client.delete_core_definition
+               )
+
+    return crhelper.cfn_handler(event, context,
+                                handler.create, handler.update, handler.delete,
+                                logger, init_failed)
+
+def function_handler(event, context):
+    ''' Lambda handler to manage AWS Greengrass FunctionDefinition resources. '''
+    global logger
+    logger = crhelper.log_config(event)
+
+    def clean_func(function):
+        res = function
+        res = keypath.replace(res, 'FunctionConfiguration.Environment.AccessSysfs',
+                              lambda e: val_to_bool(e), inline=False)
+        res = keypath.replace(res, 'FunctionConfiguration.Pinned',
+                              lambda e: val_to_bool(e), inline=False)
+        res = keypath.replace(res, 'FunctionConfiguration.MemorySize',
+                              lambda e: int(e), inline=False)
+        res = keypath.replace(res, 'FunctionConfiguration.Timeout',
+                              lambda e: int(e), inline=False)
+        return res
+
+    handler = CollectionHandler(
+                    logger, 'Functions', 'FunctionDefinitionId',
+                    clean_func,
+                    greengrass_client.create_function_definition,
+                    greengrass_client.create_function_definition_version,
+                    greengrass_client.update_function_definition,
+                    greengrass_client.delete_function_definition
+               )
+
+    return crhelper.cfn_handler(event, context,
+                                handler.create, handler.update, handler.delete,
+                                logger, init_failed)
+
+def logger_handler(event, context):
+    ''' Lambda handler to manage AWS Greengrass LoggerDefinition resources. '''
+    global logger
+    logger = crhelper.log_config(event)
+
+    def clean_logger(logger_def):
+        return keypath.replace(logger_def, 'Space', lambda e: int(e), inline=False)
+
+    handler = CollectionHandler(
+                    logger, 'Loggers', 'LoggerDefinitionId',
+                    clean_logger,
+                    greengrass_client.create_logger_definition,
+                    greengrass_client.create_logger_definition_version,
+                    greengrass_client.update_logger_definition,
+                    greengrass_client.delete_logger_definition
+               )
+
+    return crhelper.cfn_handler(event, context,
+                                handler.create, handler.update, handler.delete,
+                                logger, init_failed)
+
+def resource_handler(event, context):
+    ''' Lambda handler to manage AWS Greengrass ResourceDefinition resources. '''
+    global logger
+    logger = crhelper.log_config(event)
+
+    def clean_res(resource):
+        res = resource
+        res = keypath.replace(res,
+                              'ResourceDataContainer.LocalDeviceResourceData.GroupOwnerSetting.AutoAddGroupOwner',
+                              lambda e: val_to_bool(e),
+                              inline=False)
+        res = keypath.replace(res,
+                              'ResourceDataContainer.LocalVolumeResourceData.GroupOwnerSetting.AutoAddGroupOwner',
+                              lambda e: val_to_bool(e),
+                              inline=False)
+        return res
+
+    handler = CollectionHandler(
+                    logger, 'Resources', 'ResourceDefinitionId',
+                    clean_res,
+                    greengrass_client.create_resource_definition,
+                    greengrass_client.create_resource_definition_version,
+                    greengrass_client.update_resource_definition,
+                    greengrass_client.delete_resource_definition
+               )
+
+    return crhelper.cfn_handler(event, context,
+                                handler.create, handler.update, handler.delete,
+                                logger, init_failed)
+
+def subscription_handler(event, context):
+    ''' Lambda handler to manage AWS Greengrass SubscriptionDefinition resources. '''
+    global logger
+    logger = crhelper.log_config(event)
+
+    def clean_sub(subscription):
+        return subscription
+
+    handler = CollectionHandler(
+                    logger, 'Subscriptions', 'SubscriptionDefinitionId',
+                    clean_sub,
+                    greengrass_client.create_subscription_definition,
+                    greengrass_client.create_subscription_definition_version,
+                    greengrass_client.update_subscription_definition,
+                    greengrass_client.delete_subscription_definition
+               )
+
+    return crhelper.cfn_handler(event, context,
+                                handler.create, handler.update, handler.delete,
+                                logger, init_failed)
+
+def device_handler(event, context):
+    ''' Lambda handler to manage AWS Greengrass DeviceDefinition resources. '''
+    global logger
+    logger = crhelper.log_config(event)
+
+    def clean_device(device):
+        return keypath.replace(device, 'SyncShadow', lambda e: val_to_bool(e), inline=False)
+
+    handler = CollectionHandler(
+                    logger, 'Devices', 'DeviceDefinitionId',
+                    clean_core,
+                    greengrass_client.create_device_definition,
+                    greengrass_client.create_device_definition_version,
+                    greengrass_client.update_device_definition,
+                    greengrass_client.delete_device_definition
+               )
+
+    return crhelper.cfn_handler(event, context,
+                                handler.create, handler.update, handler.delete,
+                                logger, init_failed)
